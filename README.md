244b4c8583f0301b831d220867df8e099c8d2a45
lin-1214 <b10901059@g.ntu.edu.tw>
2024-11-06 11:15:59 +0800

    modify bash script

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 2024Econometric
+
+## genHw.sh
+
+```bash
+# Generate a new homework with csv file
+./genHw.sh -n <hw_number> -c
+
+# Generate a new homework without csv file
+./genHw.sh -n <hw_number>
+```
--- genHw.sh
+++ genHw.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "[-]Usage: genHw.sh <hw_number>"
+while getopts "n:c" flag; do
+    case "${flag}" in
+        n) hw_number=${OPTARG};;
+        c) copy_csv=true;;
+        *) echo "[-]Usage: genHw.sh -n <hw_number> [-c]"
+           exit 1;;
+    esac
+done
+
+if [ -z "$hw_number" ]; then
+    echo "[-]Usage: genHw.sh -n <hw_number> [-c]"
     exit 1
 fi
 
-mkdir hw$1
-
-cd hw$1
-touch homework$1.r
+mkdir hw$hw_number
+cd hw$hw_number
+touch homework$hw_number.r
 touch README.md
 touch Report.md
-cp ../Equity_Premium.csv ./
+
+if [ "$copy_csv" = true ]; then
+    cp ../Equity_Premium.csv ./
+fi
